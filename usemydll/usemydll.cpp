5ad459728be199f782fbe7c452f17cf6de3edc37
Roi-Intelichain <Roi.n@Inteli-chain.com>
2025-06-30 20:51:09 +0300

    Add mythreadfunction

--- usemydll/usemydll.cpp
+++ usemydll/usemydll.cpp
@@ -4,6 +4,8 @@
 //#define LIBRARY "C:\\Users\\roini\\Downloads\\injection\\injection\\my_awesome_dll.dll"
 
 typedef void(*PFUNC)(void);
+DWORD WINAPI MyThreadFunction(LPVOID lpParam);
+
 
 int main()
 {
@@ -22,7 +24,6 @@ int main()
 		return 1;
 	}
 	*/
-	
 
 	HANDLE processHandle = OpenProcess(THREAD_ALL_ACCESS, true, 30020);
 	std::cout << "Process handle: " << processHandle << "\n";
@@ -30,22 +31,19 @@ int main()
 	std::cout << "securityDescriptor: " << securityDescriptor << "\n";
 	SIZE_T stackSize = 150000;
 	std::cout << "stackSize: " << stackSize << "\n";
-	LPTHREAD_START_ROUTINE appDefinedFunc = (LPTHREAD_START_ROUTINE)&LoadLibraryA;
-	std::cout << "appDefinedFunc: " << appDefinedFunc << "\n";
 	CONST CHAR* argText = LIBRARY;
-	LPVOID arg = LPVOID(argText);
-	appDefinedFunc(arg);
+	LPVOID arg = (LPVOID) argText;
 	std::cout << "arg: " << arg << "\n";
-	DWORD flags = NULL;
+	DWORD flags = 0;
 	LPDWORD threadID = NULL;
 
-	HANDLE threaHandle = CreateRemoteThread(processHandle, securityDescriptor, stackSize, appDefinedFunc, arg, flags, threadID);
+	HANDLE threaHandle = CreateRemoteThread(processHandle, securityDescriptor, stackSize, MyThreadFunction, arg, flags, threadID);
 	printf("The error code is: %d\n", GetLastError());
 	std::cout << "thread id: " << threadID << "\n";
 	std::cout << "thread handle: " << threaHandle << "\n";
 	std::cout << "thread id with function: " << GetThreadId(threaHandle) << "\n";
 
-	//CloseHandle(processHandle); //TODO don't forget to close the handle
+	CloseHandle(processHandle); //TODO don't forget to close the handle
 	
 
 	//std::cout << "Which process would you like to inject into?(process id)"
@@ -53,5 +51,11 @@ int main()
 	//std::cin >> 
 	//BOOL injectDLL = WriteProcessMemory(hProcess, lpBaseAdderess, pFunc, sizeof(pFunc), NULL);
 
+	return 0;
+}
+
+DWORD WINAPI MyThreadFunction(LPVOID lpParam)
+{
+	LoadLibraryA(static_cast<LPCSTR>(lpParam));
 	return 0;
 }
